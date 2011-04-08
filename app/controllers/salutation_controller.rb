a23071ceef62ade08916d464862a34747cf5cc28
niraj lulay <nirajlulay@csu.fullerton.edu>
2011-04-07 21:21:31 -0700

    first commit

---
+++ app/controllers/salutation_controller.rb
@@ -0,0 +1,12 @@
+class SalutationController < ApplicationController
+def hello 
+   
+ @message = 'Hello World!' 
+  
+ end  
+def valid_html 
+   
+ @validated = 'Valid Action Generated with HTML5 version!' 
+  
+ end  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = 'd482bac5133837fd6ed797317cf6bf791a74a9e271fc571b15058a5c695d75d42647a6e56fa9ed861da7d3f5eca197191f00847de2015854639f72844be7d965'
